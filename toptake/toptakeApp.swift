5c28ce53eced82978bd724b045d683280fc935e3
Lindsey Stevens <lstevens@tra.com>
2025-09-05 15:23:12 -0700

    Initial Commit

---
+++ toptake/toptakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  toptakeApp.swift
+//  toptake
+//
+//  Created by Lindsey Stevens on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct toptakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
